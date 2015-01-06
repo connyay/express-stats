efa59e7bed89bf9013bf356ecd6c2daa3888bfa7
Connor Hindley <connor@redhat.com>
2015-01-06 12:26:11 -0500

    test internal middleware

--- api/stat/index.js
+++ api/stat/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var utils = require('rh-node-utils');
 var controller = require('./stat.controller');
 
 var router = express.Router();
@@ -8,6 +9,6 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/badge', controller.badge);
 router.get('/count', controller.count);
-router.post('/', controller.create);
+router.post('/', utils.internal(), controller.create);
 
 module.exports = router;
