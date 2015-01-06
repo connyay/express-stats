da215d89de49cf2aa86b6dacd6a262a28caeeecd
Connor Hindley <connor@redhat.com>
2015-01-06 12:59:35 -0500

    add ext

--- api/stat/index.js
+++ api/stat/index.js
@@ -7,7 +7,7 @@ var controller = require('./stat.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
-router.get('/badge', controller.badge);
+router.get('/badge.svg', controller.badge);
 router.get('/count', controller.count);
 router.post('/', utils.internal(), controller.create);
 
